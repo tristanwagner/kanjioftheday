ed19dae930d43aa55699453ef8d8f264a4dea297
Tristan WAGNER <t.wagner66250@gmail.com>
2024-10-26 11:35:07 +0200

    filter out kanji without meaning

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-<h1 align="center">Kanji of the day</h1><h1 align="center">倗</h1><p align="center">meaning(s): </p><p align="center">ON reading(s): <b>ホウ</b></p><p align="center">KUN reading(s): <b>たす.ける, ゆだ.ねる, よ.せる</b></p><p align="center">stroke count: <b>10</b></p><p align="center">unicode: <b>5017</b></p><p align="center"><img src="https://komarev.com/ghpvc/?username=tristanwagner-kanjioftheday&label=Views&color=0e75b6&style=flat" alt="views"/></p>
+<h1 align="center">Kanji of the day</h1>
+<h1 align="center">嚕</h1>
+<p align="left">meaning(s): mumbling, talkative, verbose</p>
+<p align="left">ON reading(s): <b>ロ</b></p>
+<p align="left">KUN reading(s): <b>かた.る, へつら.う</b></p>
+<p align="left">stroke count: <b>18</b></p>
+<p align="left">unicode: <b>5695</b></p>
+<p align="left"><img src="https://komarev.com/ghpvc/?username=tristanwagner-kanjioftheday&label=Views&color=0e75b6&style=flat" alt="views"/></p>
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const header = '<h1 align="center">Kanji of the day</h1>';
 let output = '';
 const add = (input) => {
   output += input;
+  output += '\n';
 }
 
 const getKanji = () => {
@@ -16,40 +17,57 @@ const getKanji = () => {
   return kanji[diff % kanji.length];
 }
 
+const fetchKanji = async () => {
+  let data;
+
+  // it's possible that meanings is empty
+  // and we don't want that
+  while (!data?.meanings?.length) {
+    data = await fetch(`https://kanjiapi.dev/v1/kanji/${getKanji()}`)
+      .then((r) => r.json());
+  }
+
+  return data;
+}
+
 (async () => {
-  const data = await fetch(`https://kanjiapi.dev/v1/kanji/${getKanji()}`).then((r) => r.json());
+  let data = await fetchKanji()
 
   add(header);
   add(`<h1 align="center">${data.kanji}</h1>`);
-  add(`<p align="center">meaning(s): ${data.meanings.join(', ')}</p>`);
+  add(`<p align="left">meaning(s): ${data.meanings.join(', ')}</p>`);
+
+  if (data.heisig_en) {
+    add(`<p align="left">Heisig: <b>${data.heisig_en}</b></p>`);
+  }
 
   if (data.on_readings?.length) {
-    add(`<p align="center">ON reading(s): <b>${data.on_readings.join(', ')}</b></p>`);
+    add(`<p align="left">ON reading(s): <b>${data.on_readings.join(', ')}</b></p>`);
   }
 
   if (data.kun_readings?.length) {
-    add(`<p align="center">KUN reading(s): <b>${data.kun_readings.join(', ')}</b></p>`);
+    add(`<p align="left">KUN reading(s): <b>${data.kun_readings.join(', ')}</b></p>`);
   }
 
   if (data.name_readings?.length) {
-    add(`<p align="center">name reading(s): <b>${data.name_readings.join(', ')}</b></p>`);
+    add(`<p align="left">name reading(s): <b>${data.name_readings.join(', ')}</b></p>`);
   }
 
   if (data.freq_mainichi_shinbun) {
-    add(`<p align="center">frequency: <b>${data.freq_mainichi_shinbun}</b></p>`);
+    add(`<p align="left">frequency: <b>${data.freq_mainichi_shinbun}</b></p>`);
   }
 
   if (data.grade) {
-    add(`<p align="center">grade: <b>${data.grade}</b></p>`);
+    add(`<p align="left">grade: <b>${data.grade}</b></p>`);
   }
 
   if (data.jlpt) {
-    add(`<p align="center">JLPT level: <b>${data.jlpt}</b></p>`);
+    add(`<p align="left">JLPT level: <b>${data.jlpt}</b></p>`);
   }
 
-  add(`<p align="center">stroke count: <b>${data.stroke_count}</b></p>`);
-  add(`<p align="center">unicode: <b>${data.unicode}</b></p>`);
-  add(`<p align="center"><img src="https://komarev.com/ghpvc/?username=tristanwagner-kanjioftheday&label=Views&color=0e75b6&style=flat" alt="views"/></p>`)
+  add(`<p align="left">stroke count: <b>${data.stroke_count}</b></p>`);
+  add(`<p align="left">unicode: <b>${data.unicode}</b></p>`);
+  add(`<p align="left"><img src="https://komarev.com/ghpvc/?username=tristanwagner-kanjioftheday&label=Views&color=0e75b6&style=flat" alt="views"/></p>`)
 
   await promises.writeFile("README.md", output);
 })();
